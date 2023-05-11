36d93cbf1d05f209a462799cbbffa67e313301b9
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-05-11 14:05:14 -0500

    Update
    Creating HTML file automatic using webpack configs

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: 'index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'To-Do List',
+    inject: 'div'
+  })],
+};
