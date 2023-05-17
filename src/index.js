ae81020bc988a4c593ff0bcf4bb518911bca92d5
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-05-16 19:13:41 -0500

    Update
    Creating a function to iterate objects

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import './style.css'
-const speakersdata = [
+const task = [
   {
     description: '',
     completed: false,
     index: 0
   }
 ];
-let i = 0;
-const listcontainer = document.querySelector('ListContainer');
-const AddTask = () => {
-  
-};
+
+const listcontainer = document.querySelector('.ListContainer');
+const AddNewTask = document.querySelector('.Add');
+
+
+
+AddNewTask.addEventListener('click', (event) => {
+  event.preventDefault();
+  const taskinput = document.getElementById('TaskInput').value;
+  let i = 0;
+  task[i].index = (i += 1);
+  task[i].description = taskinput;
+  const taskcontainer = document.createElement('li');
+  taskcontainer.className = 'task';  
+  listcontainer.append(taskcontainer);
+  taskcontainer.innerHTML=`
+<div class="TaskTittleContainer">
+  <input type="checkbox">
+  <h3 class="TaskName" id=task${i}></h3>
+</div>
+<button class="upload"></button>
+  `;
+  const TaskName = document.querySelector('.TaskName');
+  TaskName.textContent = speakersdata[i].description; 
+  i += 1;               
+});
+
