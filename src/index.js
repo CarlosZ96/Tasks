a98388d93101df2d168daf4e9ee23b6f6fee98b3
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-05-16 22:01:28 -0500

    Update
    Fix iterate fuction

--- src/index.js
+++ src/index.js
@@ -1,35 +1,30 @@
 import './style.css'
-const task = [
-  {
-    description: '',
-    completed: false,
-    index: 0
-  }
-];
+let task = [];
 
 const listcontainer = document.querySelector('.ListContainer');
 const AddNewTask = document.querySelector('.Add');
-
-
+let i = 0;
 
 AddNewTask.addEventListener('click', (event) => {
   event.preventDefault();
   const taskinput = document.getElementById('TaskInput').value;
-  let i = 0;
-  task[i].index = (i += 1);
-  task[i].description = taskinput;
+  let iscomplete = false;
+  const newtask = {taskinput, iscomplete, i};
+  task.push(newtask);
   const taskcontainer = document.createElement('li');
   taskcontainer.className = 'task';  
   listcontainer.append(taskcontainer);
   taskcontainer.innerHTML=`
 <div class="TaskTittleContainer">
   <input type="checkbox">
-  <h3 class="TaskName" id=task${i}></h3>
+  <h3 class="TaskName"</h3>
 </div>
 <button class="upload"></button>
   `;
   const TaskName = document.querySelector('.TaskName');
-  TaskName.textContent = speakersdata[i].description; 
-  i += 1;               
+  TaskName.textContent = task[i].taskinput; 
+  console.log(task); 
+  task[i].i = i;
+  i += 1;        
 });
 
