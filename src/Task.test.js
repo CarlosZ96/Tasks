5152a0b7e7a9f015fbe3746b5b90f8251525b336
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-07-27 13:22:06 -0500

    Mock
    Added Mock for HTML and to see if delete and Add task is working

--- src/Task.test.js
+++ src/Task.test.js
@@ -1,44 +1,70 @@
 import { Tasks } from './task.js';
+import { JSDOM } from 'jsdom';
 
 describe('Tasks', () => {
   let taskso;
-
+  let document;
+  let listContainer;
   beforeEach(() => {
     taskso = new Tasks();
-    taskso.tasks = []; // Ensure tasks array is empty before each test
+    taskso.tasks = [];
+
+    // Create a mock DOM environment using jsdom
+    const dom = new JSDOM(`
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>To-Do List</title>
+      </head>
+      <body>
+        <section class="TdContainer">
+          <form class="TaskListContainer">
+            <ul class="TittleContainer">
+              <li><h3 class="Ttittle">Today's To Do</h3></li>
+              <li><button class="upload"></button></li>
+            </ul>
+            <ul class="AddContainer">
+              <li><input type="text" placeholder="Add to you List..." id="TaskInput" name="addtaskn"></li>
+              <li><button type="submit" class="Add"></button></li>
+            </ul>
+            <ul class="ListContainer">
+            </ul>
+            <div class="clear">
+              <button class="ClearButton">Clear all completed</button>
+            </div>
+          </form>
+        </section>
+      </body>
+      </html>
+    `);
+    document = dom.window.document;
+    listContainer = document.querySelector('.ListContainer');
   });
 
   describe('AddTask(description)', () => {
     it('should add a new task with the given description', () => {
       const description = 'Buy groceries';
       taskso.AddTask(description);
-
-      // Check if the task was added correctly
-      expect(taskso.tasks).toHaveLength(1);
-      expect(taskso.tasks[0].description).toBe(description);
-      expect(taskso.tasks[0].iscomplete).toBeFalsy(); // New tasks are not completed
+      const liElements = listContainer.querySelectorAll('li');
+      expect(liElements).toHaveLength(1);
+      expect(liElements[0].textContent).toBe(description);
     });
-
-    // Add more test cases for different scenarios, if needed
   });
-
   describe('DeleteTask(id)', () => {
     it('should delete the task with the given id', () => {
-      // Add some tasks to the tasks array
       taskso.tasks.push(
         { id: 1, description: 'Task 1', iscomplete: false },
         { id: 2, description: 'Task 2', iscomplete: true },
         { id: 3, description: 'Task 3', iscomplete: false }
       );
-
       const taskIdToDelete = 2;
       taskso.DeleteTask(taskIdToDelete);
-
-      // Check if the task was deleted correctly
-      expect(taskso.tasks).toHaveLength(2);
-      expect(taskso.tasks.some(task => task.id === taskIdToDelete)).toBeFalsy();
+      const liElements = listContainer.querySelectorAll('li');
+      expect(liElements).toHaveLength(2);
+      expect(Array.from(liElements).some(li => li.textContent === 'Task 2')).toBeFalsy();
     });
-
-    // Add more test cases for different scenarios, if needed
   });
 });
