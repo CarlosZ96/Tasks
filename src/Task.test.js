be8ebb86269248adbdaa41019ef64a74666da4ca
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-07-27 12:54:23 -0500

    fix Testing
    Fixing delete testing fuction

--- src/Task.test.js
+++ src/Task.test.js
@@ -1,31 +1,44 @@
 import { Tasks } from './task.js';
-describe('Tasks class', () => {
-  let tasks;
+
+describe('Tasks', () => {
+  let taskso;
+
   beforeEach(() => {
-    tasks = new Tasks();
-    localStorage.clear();
+    taskso = new Tasks();
+    taskso.tasks = []; // Ensure tasks array is empty before each test
   });
-  describe('AddTask', () => {
-    test('should add a new task to the tasks list', () => {
-      tasks.AddTask('Task 1');
-      tasks.AddTask('Task 2');
-      const storedTasks = JSON.parse(localStorage.getItem('tasks'));
-      expect(storedTasks).toHaveLength(2);
-      expect(storedTasks[0].description).toBe('Task 1');
-      expect(storedTasks[1].description).toBe('Task 2');
+
+  describe('AddTask(description)', () => {
+    it('should add a new task with the given description', () => {
+      const description = 'Buy groceries';
+      taskso.AddTask(description);
+
+      // Check if the task was added correctly
+      expect(taskso.tasks).toHaveLength(1);
+      expect(taskso.tasks[0].description).toBe(description);
+      expect(taskso.tasks[0].iscomplete).toBeFalsy(); // New tasks are not completed
     });
-  })
+
+    // Add more test cases for different scenarios, if needed
+  });
+
   describe('DeleteTask(id)', () => {
     it('should delete the task with the given id', () => {
+      // Add some tasks to the tasks array
       taskso.tasks.push(
         { id: 1, description: 'Task 1', iscomplete: false },
         { id: 2, description: 'Task 2', iscomplete: true },
         { id: 3, description: 'Task 3', iscomplete: false }
       );
+
       const taskIdToDelete = 2;
       taskso.DeleteTask(taskIdToDelete);
+
+      // Check if the task was deleted correctly
       expect(taskso.tasks).toHaveLength(2);
       expect(taskso.tasks.some(task => task.id === taskIdToDelete)).toBeFalsy();
     });
+
+    // Add more test cases for different scenarios, if needed
   });
 });
