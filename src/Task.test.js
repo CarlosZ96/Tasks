093fb765f5efcfa0ee209543c48c99d9f0780ecd
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-07-27 18:30:45 -0500

    Update Task.test.js
    Fix Linters

--- src/Task.test.js
+++ src/Task.test.js
@@ -3,7 +3,7 @@ describe('Tasks', () => {
   let taskso;
   beforeEach(() => {
     taskso = new Tasks();
-    taskso.tasks = []; 
+    taskso.tasks = [];
   });
   describe('AddTask(description)', () => {
     it('should add a new task with the given description', () => {
@@ -11,7 +11,7 @@ describe('Tasks', () => {
       taskso.AddTask(description);
       expect(taskso.tasks).toHaveLength(1);
       expect(taskso.tasks[0].description).toBe(description);
-      expect(taskso.tasks[0].iscomplete).toBeFalsy(); 
+      expect(taskso.tasks[0].iscomplete).toBeFalsy();
     });
   });
   describe('DeleteTask(id)', () => {
@@ -19,7 +19,7 @@ describe('Tasks', () => {
       taskso.tasks.push(
         { id: 1, description: 'Task 1', iscomplete: false },
         { id: 2, description: 'Task 2', iscomplete: true },
-        { id: 3, description: 'Task 3', iscomplete: false }
+        { id: 3, description: 'Task 3', iscomplete: false },
       );
       const taskIdToDelete = 2;
       taskso.DeleteTask(taskIdToDelete);
