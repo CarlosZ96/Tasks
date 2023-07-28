790585f9f478d1e32dfcfc816bc74eff8d33e505
CarlosZ96 <c4r1oszambrano@gmail.com>
2023-07-27 20:44:28 -0500

    Update Task.test.js
    Using the describe method it is tested if when creating a new description (task) it is created and changed correctly and in it, the corresponding ID. In the same way, it is evaluated if the status of whether the task is complete or not is updated with the checkbox and also when calling the method to delete any task with its "incomplete" complete attribute.

--- src/Task.test.js
+++ src/Task.test.js
@@ -1,31 +1,77 @@
 import { Tasks } from './task.js';
 
 describe('Tasks', () => {
-  let taskso;
   beforeEach(() => {
-    taskso = new Tasks();
-    taskso.tasks = [];
+    localStorage.clear();
   });
-  describe('AddTask(description)', () => {
-    it('should add a new task with the given description', () => {
-      const description = 'Buy groceries';
-      taskso.AddTask(description);
-      expect(taskso.tasks).toHaveLength(1);
-      expect(taskso.tasks[0].description).toBe(description);
-      expect(taskso.tasks[0].iscomplete).toBeFalsy();
+  describe('AddTask', () => {
+    it('should add a new task to the list', () => {
+      const taskso = new Tasks();
+      taskso.AddTask('Task 1');
+      taskso.AddTask('Task 2');
+      const tasks = JSON.parse(localStorage.getItem('tasks'));
+      expect(tasks).toHaveLength(2);
+      expect(tasks[0].description).toBe('Task 1');
+      expect(tasks[1].description).toBe('Task 2');
     });
   });
-  describe('DeleteTask(id)', () => {
-    it('should delete the task with the given id', () => {
-      taskso.tasks.push(
-        { id: 1, description: 'Task 1', iscomplete: false },
-        { id: 2, description: 'Task 2', iscomplete: true },
-        { id: 3, description: 'Task 3', iscomplete: false },
-      );
-      const taskIdToDelete = 2;
-      taskso.DeleteTask(taskIdToDelete);
-      expect(taskso.tasks).toHaveLength(2);
-      expect(taskso.tasks.some((task) => task.id === taskIdToDelete)).toBeFalsy();
+  describe('DeleteTask', () => {
+    it('should delete the task with the specified id', () => {
+      const taskso = new Tasks();
+      taskso.AddTask('Task 1');
+      taskso.AddTask('Task 2');
+      taskso.AddTask('Task 3');
+      let tasks = JSON.parse(localStorage.getItem('tasks'));
+      taskso.DeleteTask(2);
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+      expect(tasks).toHaveLength(2);
+      expect(tasks.map(task => task.description)).toEqual(['Task 1', 'Task 3']);
+    });
+    it('should not delete anything if the id does not exist', () => {
+      const taskso = new Tasks();
+      taskso.AddTask('Task 1');
+      taskso.AddTask('Task 2');
+      let tasks = JSON.parse(localStorage.getItem('tasks'));
+      taskso.DeleteTask(10);
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+      expect(tasks).toHaveLength(2);
+      expect(tasks.map((task) => task.description)).toEqual(['Task 1', 'Task 2']);
+    });
+  });
+  describe('EditTask', () => {
+    it('should update the description of the task with the specified id', () => {
+      const taskso = new Tasks();
+      taskso.AddTask('Task 1');
+      let tasks = JSON.parse(localStorage.getItem('tasks'));
+      taskso.EditTask(1, 'Updated Task');
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+      expect(tasks[0].description).toBe('Updated Task');
+    });
+    it('should not update anything if the id does not exist', () => {
+      const taskso = new Tasks();
+      taskso.AddTask('Task 1');
+      let tasks = JSON.parse(localStorage.getItem('tasks'));
+      taskso.EditTask(10, 'Updated Task');
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+      expect(tasks[0].description).toBe('Task 1');
+    });
+  });
+  describe('CheckTask', () => {
+    it('should update the iscomplete status of the task with the specified id', () => {
+      const taskso = new Tasks();
+      taskso.AddTask('Task 1');
+      let tasks = JSON.parse(localStorage.getItem('tasks'));
+      taskso.CheckTask(1, true);
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+      expect(tasks[0].iscomplete).toBe(true);
+    });
+    it('should not update anything if the id does not exist', () => {
+      const taskso = new Tasks();
+      taskso.AddTask('Task 1');
+      let tasks = JSON.parse(localStorage.getItem('tasks'));
+      taskso.CheckTask(10, true);
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+      expect(tasks[0].iscomplete).toBe(false);
     });
   });
 });
